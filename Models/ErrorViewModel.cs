217b6137dfaa4cbd698ea1e1e576205a9c265d87
Sena Kuzğu <155326583+senakzgu@users.noreply.github.com>
2025-11-27 23:19:44 +0300

    commit 4
    iyileştirmeler yapıldı

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FitnessApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Models/Gym.cs
+++
@@ -1,9 +0,0 @@
-namespace FitnessApp.Models
-{
-    public class Gym
-    {
-        public int Id { get; set; }             // Primary Key
-        public string? Name { get; set; }        // Salon adı
-        public string? WorkingHours { get; set; } // Örn: “08:00 - 22:00”
-    }
-}
